7d883974c53c38ffecefab659d58fe702775f282
Jabanom <81070152+Jabanom@users.noreply.github.com>
2022-06-14 15:04:42 +1000

    Comment box
    first stage of comment box

---
+++ n10484035_Roberts/CommentScript.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    function addComment(comment) {
+      var Comments = document.querySelectorAll(".Comment");
+      var curr = document.createElement("div");
+      curr.setAttribute("class", "Comment active");
+      var p1 = document.createElement("p");
+      p1.setAttribute("class", "Comment");
+      p1.innerHTML = comment;
+      curr.appendChild(p1);
+      if (Comments.length) {
+        Comments[Comments.length -1].insertAdjacentHTML("afterEnd", curr.outerHTML)
+      } else {
+        document.forms[0].insertAdjacentHTML("afterEnd", curr.outerHTML)
+      }
+    }
+  
+    var obj = {
+      Comment: "",
+    };
+  
+    document.forms[0].onchange = function(e) {
+      obj[e.target.name] = e.target.value;
+    }
+
+    document.forms[0].onsubmit = function(e) {
+      e.preventDefault();
+      addComment(obj.Comment)
+      Commented = true;
+    }
+  }
