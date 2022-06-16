db38c9026a67caaa5446b92d6d58ca25de49ec22
Zara <106126616+ZRoberts01@users.noreply.github.com>
2022-06-16 17:27:45 +1000

    Add files via upload

---
+++ IAB207-Group11-main/README.md
@@ -0,0 +1,10 @@
+# iab207
+This is the group repo for iab207 assignment
+
+Tutorial: Tues 3-5 Cameron
+
+Group 11:
+Danny Bayton,
+Justin Deeb,
+Darcy Gray,
+Zara Roberts
---
+++ IAB207-Group11-main/User/models.py
@@ -0,0 +1,12 @@
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    address = db.Column(db.String(100), index=True, unique=True, nullable=False)
