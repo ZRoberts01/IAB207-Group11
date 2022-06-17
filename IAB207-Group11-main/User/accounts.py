6090fae12f9eb4216f42bf5d8a698e3be3672bf8
ChairdolfSitler <107556076+ChairdolfSitler@users.noreply.github.com>
2022-06-17 15:53:34 +1000

    Delete User Folder
    Folder not needed

--- IAB207-Group11-main/User/accounts.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('registration', __name__, url_prefix='/templates')
-
-@bp.route('/<id>')
-def register(id):
-    return render_template('templates/registration.html')
