2437f1b7db1073823172cc03d6f51a9032516433
ChairdolfSitler <107556076+ChairdolfSitler@users.noreply.github.com>
2022-06-18 14:10:51 +1000

    Form Update
    New parameters

--- IAB207-Group11-main/gamingevents/forms.py
+++ IAB207-Group11-main/gamingevents/forms.py
@@ -3,19 +3,44 @@ from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordFiel
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
-ALLOWED_FILE = {"jpg, jpeg, PNG, png, JPG"}
+ALLOWED_FILE = {'jpg', 'jpeg', 'PNG', 'png', 'JPG'}
+
+#Create Event Form
+class EventForm(FlaskForm):
+    eventName = StringField('Event Name', validators=[InputRequired()])
+    description = TextAreaField('Event Description', validators=[InputRequired()])
+    image = FileField('Event Banner', validators= [FileRequired(message= 'Please add file'), 
+                                                    FileAllowed(ALLOWED_FILE, message= 'Only supports, jpg, jpeg, PNG, png, JPG')])
+    category = StringField('Event Category', validators=[InputRequired()])
+    location = StringField('Event Address', validators=[InputRequired()])
+    creatorName = StringField('Company/Creator Name', validators=[InputRequired()])
+    email = StringField('Email Address', validators=[InputRequired()])
+    date = DateField('Event Date', validators=[InputRequired()])
+    time = TimeField('Event Time', validators=[InputRequired()])
+    ticketlimit = IntegerField('Booking Limit', validators=[InputRequired("Please use numbers only")])
+    price = StringField('Ticket Price', validators=[InputRequired()])
+    t_d = BooleanField('I accept all terms and conditions', validators=[InputRequired()])
+    submit = SubmitField('Create Event')
+
+#Comment
+class CommentsForm(FlaskForm):
+    text = TextAreaField('Comment', [InputRequired()])
+    submit = SubmitField('Post')
+
+
 
 
 #Login Form
 class LoginForm(FlaskForm):
     user_name=StringField("Username", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    password=PasswordField("Password", validators=[InputRequired('Enter password')])
     submit=SubmitField("Login")
 
 #Register Form
 class RegisterForm(FlaskForm):
     user_Name=StringField("User Name", validators=[InputRequired()])
-
+    first_name=StringField("First Name", validators=[InputRequired("Please enter first name")])
+    last_name=StringField("Last Name", validators=[InputRequired("Please enter last name")])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
     #Password Creation and confirmation
     password=PasswordField("Password", validators=[InputRequired(),
@@ -28,19 +53,3 @@ class RegisterForm(FlaskForm):
     #Submit Details
     submit = SubmitField("Register")
 
-#Create Event Form
-class EventForm(FlaskForm):
-    creatorName = StringField('Company/Creator Name', validators=[InputRequired()])
-    email = StringField('Email Address', validators=[InputRequired()])
-    eventName = StringField('Event Name', validators=[InputRequired()])
-    image = FileField('Event Banner', validators=[FileRequired(message = 'Image Cannot be empty'), 
-                                                  FileAllowed(ALLOWED_FILE, message= 'Only supports, jpg, jpeg, PNG, png, JPG')])
-    category = StringField('Event Category', validators=[InputRequired()])
-    description = TextAreaField('Event Description', validators=[InputRequired()])
-    date = DateField('Event Date', validators=[InputRequired()])
-    time = TimeField('Event Time', validators=[InputRequired()])
-    location = StringField('Event Address', validators=[InputRequired()])
-    ticketlimit = IntegerField('Booking Limit', validators=[InputRequired("Please use numbers only")])
-    price = StringField('Ticket Price', validators=[InputRequired()])
-    t_d = BooleanField('I accept all terms and conditions', validators=[InputRequired()])
-    submit = SubmitField('Create Event')
